a5899905186f321163cc6c24534383dce446a2fe
Rombusz <zf950705@gmail.com>
2019-08-08 17:19:10 +0200

    bugfix added to build scripts

--- qmake.sh
+++ qmake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-qmake /home/ferenc/BME/Onlab1/2dpainting/2dpainting.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug
-make -f /home/ferenc/BME/Onlab1/2dpainting/Build/Makefile qmake_all
+/opt/Qt/5.12.1/gcc_64/bin/qmake /home/ferenc/BME/Onlab1/2dpainting_vscode/2dpainting.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug
+make -f /home/ferenc/BME/Onlab1/2dpainting_vscode/Build/Makefile qmake_all
